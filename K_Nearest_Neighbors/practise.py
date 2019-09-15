1bce483900875b3e8191bdf0354406e266675768
hendrik.webdeveloper-at-708977417062 <hendrik@Hendriks-MacBook-Pro.local>
2019-09-15 12:44:30 +0800

    Logistic Regression, KNN, SVM

--- K_Nearest_Neighbors/practise.py
+++ K_Nearest_Neighbors/practise.py
@@ -1,48 +1,42 @@
 # -*- coding: utf-8 -*-
 
-import pandas as pd
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
-#importing the dataset
+# Importing the dataset
 dataset = pd.read_csv('Social_Network_Ads.csv')
-X = dataset.iloc[:,[2,3]].values #New way to select columns
+X = dataset.iloc[:,2:4].values
 y = dataset.iloc[:,4:5].values
 
-#Splitting the dataset into the Training set and Test set
+# Splitting the dataset
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
-#Running feature scaling
+#feature scaling
 # Running feature scaling only on X_train and X_test. we dont need to run for y, since its just 0 and 1
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.fit_transform(X_test)
 
-#Fitting linear regression into the training set
-from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression(random_state = 0)
+#Fitting classification into the training set
+#setting p=2 to use euclidean distance to calculate nearest neighbours
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2)
 classifier.fit(X_train, y_train)
 
-#Predicting the test set result
 y_pred = classifier.predict(X_test)
 
-#Making the confusion matrix  to evaluate the accuracy of a classification
-from sklearn.metrics import confusion_matrix #This is a function, its not all capital
+# Making the Confusion Matrix- 7 errors, better than logistic regression
+from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+"""array([[64,  4],
+       [ 3, 29]])"""
 
-"""cm
-Out[12]: 
-array([[63,  5],
-       [ 7, 25]], dtype=int64)"""
-# we have 63 + 25 correct predictions and 5 + 7 incorrect predictions
-    
-
-#Visualizing the training sets
-# Visualising the Test set results
+# Visualising the Training set results
 from matplotlib.colors import ListedColormap
-X_set, y_set = X_test, y_test
+X_set, y_set = X_train, y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
 plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
@@ -52,7 +46,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Logistic Regression (Test set)')
+plt.title('KNearestNeighbour Classifier (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
